c5d99ea959d900b65f8830cdde581b26ef6df5b6
Rodolfo Scarnatto <44070612+rdscarnatto@users.noreply.github.com>
2023-01-18 06:00:23 -0300

    Add files via upload

---
+++ STM32F1xx_drivers/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Daniel/Documents/STM32\ Projects/MCU1UdemyWS/STM32F1xx_drivers/drivers/Inc/stm32f103xx.h
+C:/Users/Daniel/Documents/STM32\ Projects/MCU1UdemyWS/STM32F1xx_drivers/drivers/Inc/stm32f103xx.h:
---
+++ STM32F1xx_drivers/drivers/Inc/stm32f103xx.h
@@ -0,0 +1,21 @@
+/*
+ * stm32f103xx.h
+ *
+ *  Created on: Jan 17, 2023
+ *      Author: Daniel
+ */
+
+#ifndef INC_STM32F103XX_H_
+#define INC_STM32F103XX_H_
+
+/*
+* Base addres of FALSH and SRAM memories
+*/
+
+#define FLASH_BASEADDR		0x80000000U
+#define SRAM_BASEADDR		0x20000000U
+
+
+
+
+#endif /* INC_STM32F103XX_H_ */
